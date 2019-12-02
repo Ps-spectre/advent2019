66113b2abed45507868e8566d7d41d17065a7457
Andrew Kishchenko <psspectre@gmail.com>
2019-12-02 22:36:16 +0200

    day1 shorter python version

--- day1/day1.py
+++ day1/day1.py
@@ -1,17 +1,14 @@
-import fileinput
+lines = [ int(x.strip()) for x in input().splitlines() ]
 
-lines = [ int(x.strip()) for x in fileinput.input() ]
-
-def fuel(x): return x // 3 - 2
+def fuel(x):
+    return x // 3 - 2
 
 def full(x, s=0):
     while True:
         f = fuel(x)
-        if f <= 0: return s
+        if f <= 0:
+            return s
         s, x = s + f, f
 
-def part1(): return sum([fuel(x) for x in lines])
-def part2(): return sum([full(x) for x in lines])
-
-print(part1())
-print(part2())
+print(sum([fuel(x) for x in lines]))
+print(sum([full(x) for x in lines]))
